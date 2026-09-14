c01a12594ac4f6a3705002214c5da199877b9c38
Parl4224h <71451131+Parl4224h@users.noreply.github.com>
2026-09-14 17:57:11 +0000

    Seed database with sample book titles and include seeding in release phase

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,19 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+[
+  "The Great Gatsby",
+  "Pride and Prejudice",
+  "1984",
+  "To Kill a Mockingbird",
+  "The Catcher in the Rye",
+  "The Hobbit",
+  "Fahrenheit 451",
+  "Jane Eyre",
+  "The Lord of the Rings",
+  "Brave New World",
+  "Little Women",
+  "The Chronicles of Narnia"
+].each do |title|
+  Book.find_or_create_by!(title: title)
+end
